28a5e0c7c2a221e163ece2c34f2c33aebb484620
Angel <angelgonzc@yahoo.com>
2023-05-16 19:22:04 +0100

    homework done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tasks, setTask] = useState(
+    [
+      { id: 1, name: "Collect yer messages", priority: "low" },
+      { id: 2, name: "Clean bathroom", priority: "high" },
+      { id: 3, name: "Car's MOT", priority: "low" },
+    ]
+  )
+
+  const [newTask, setNewTask] = useState("")
+  const [newPriority, setNewPriority] = useState("")
+
+  const completeTask = (taskId) => {
+    const newListTasks = tasks.filter((task) => task.id !== taskId)
+    setTask(newListTasks)
+  }
+
+  // const colourByPriority = (taskPriority) => {
+  //   if (taskPriority === "high") {
+  //     return "red"
+  //   } else {
+  //     return "green"
+  //   }
+  // }
+
+  const listTasks = tasks.map((task) => {
+    return (
+      <li key={task.id} className={task.priority}>
+        {task.name}
+        <button onClick={() => completeTask(task.id)}>Complete</button>
+      </li>
+    )
+  })
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const handleRadioButton = (event) => {
+    setNewPriority(event.target.value)
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const lastTask = tasks.slice(-1)
+    const newTaskObj = { id: lastTask[0].id + 1, name: newTask, priority: newPriority};
+    const nextItems = [...tasks, newTaskObj];
+    setTask(nextItems);
+    setNewTask("")
+    event.target[1].checked = false
+    event.target[2].checked = false
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yer thingies tae dae</h1>
+      <hr></hr>
+      <form onSubmit={saveNewTask}>
+        <label htmlFor='new-task'>Add a new Tae Dae</label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput} />
+        <input type="radio" id="high" name="priority" value="high" onClick={handleRadioButton}/>
+        <label htmlFor="high">High</label>
+        <input type="radio" id="low" name="priority" value="low" onClick={handleRadioButton}/>
+        <label htmlFor="low">Low</label>
+        <input type="submit" value="Save new Tae Dae" />
+      </form>
+      <ul>{listTasks}</ul>
+
     </div>
   );
 }
+
 
 export default App;
